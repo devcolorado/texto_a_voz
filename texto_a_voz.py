fb7b7da81e3c063c914d73bc6b45c80ca56d8e6d
coloradodev <fcolorado@deboraarango.edu.co>
2023-10-22 17:45:46 -0500

    Cambio falso

---
+++ texto_a_voz.py
@@ -0,0 +1,26 @@
+"""Este programa sirve para convertir un artículo existente en un 
+archivo de audio reproducible en formato mp3 por medio de la url
+de dicho articulo"""
+
+#Importamos los modulos que usaremos
+from newspaper import Article
+from gtts import gTTS
+
+#pedimos la url de la pagina cuyo texto queremos convertir a audio
+#Presuponiendo que el articulo estará en español
+articulo = Article(input("Ingresa la url de la pagina: "), language="es")
+
+#Descagarmos y analizamos el contenido del articulo
+articulo.download()
+articulo.parse()
+
+#Unimos el titulo y el contenido del articulo en una variable
+#texto = articulo.title + "\n" + articulo.text
+
+#Pasamos la variable de texto a audio con la funcion gtts
+audio = gTTS(articulo.text, lang="es")
+
+#Guardamos el audio con el formato mp3 
+audio.save("audio.mp3")
+
+
