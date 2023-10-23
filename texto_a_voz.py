61055358a87bd363eeaecc532d1327326f1975aa
coloradodev <fcolorado@deboraarango.edu.co>
2023-10-23 14:59:29 -0500

    Añadimos nltk

--- texto_a_voz.py
+++ texto_a_voz.py
@@ -5,6 +5,12 @@ de dicho articulo"""
 #Importamos los modulos que usaremos
 from newspaper import Article
 from gtts import gTTS
+import nltk
+
+#Descargamos el tokenizador nltk para ayudar al modulo newspaper con la 
+#tokenizacion del texto del articulo y su posterior conversion a texto
+#con el modulo gtts
+nltk.download("punkt")
 
 #pedimos la url de la pagina cuyo texto queremos convertir a audio
 #presuponiendo que el articulo estará en español
@@ -19,5 +25,3 @@ audio = gTTS(articulo.text, lang="es")
 
 #Guardamos el audio con el formato mp3
 audio.save("audio.mp3")
-
-
